e2b5f819602b11577b28dcb0a32739e428553ca2
DuyLuat <daoduyluat@gmail.com>
2023-06-11 16:37:55 +0700

    thêm người dùng

--- src/components/ModelAddNew.js
+++ src/components/ModelAddNew.js
@@ -2,11 +2,34 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import { toast } from "react-toastify";
+import { postCreateUser } from "../services/UserService";
 
 function ModelAddNew(props) {
   const { show, setShow } = props;
-
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
   const handleClose = () => setShow(false);
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleSaveUser = async () => {
+    let response = await postCreateUser(first_name, last_name, email);
+    setShow(false);
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    toast.success("User created successfully");
+    // console.log(">>>check response:", response);
+  };
 
   return (
     <>
@@ -17,23 +40,38 @@ function ModelAddNew(props) {
         <Modal.Body>
           <Form.Group className="mb-3">
             <Form.Label>First name:</Form.Label>
-            <Form.Control type="text" placeholder="Normal text" />
+            <Form.Control
+              type="text"
+              name="first_name"
+              required
+              onChange={handleFirstNameChange}
+            />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Last name:</Form.Label>
-            <Form.Control type="text" placeholder="Normal text" />
+            <Form.Control
+              type="text"
+              name="last_name"
+              required
+              onChange={handleLastNameChange}
+            />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Control
+              type="email"
+              name="email"
+              required
+              onChange={handleEmailChange}
+            />
           </Form.Group>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button variant="primary" onClick={handleSaveUser}>
+            Save User
           </Button>
         </Modal.Footer>
       </Modal>
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,10 @@
 import axios from "./customize_axios"; // không cần thiết phải import instance (axios chính là instance)
+
 const fetchAllUser = (page) => {
   return axios.get(`/address_info?page=${page}&limit=10`);
 };
+const postCreateUser = (first_name, last_name, email) => {
+  return axios.post("/address_info", { first_name, last_name, email });
+};
 
-export { fetchAllUser };
+export { fetchAllUser, postCreateUser };
